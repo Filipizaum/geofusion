f015fe35362f922322142f8000997f078d8ea071
Tiago Araujo <tiaguzaum_@hotmail.com>
2015-06-22 14:04:20 -0400

    adicionado função de add geo

--- test/dragmask/main.js
+++ test/dragmask/main.js
@@ -17,13 +17,39 @@ function main() {
 
     var display = gamejs.display.getSurface();
     var img = gamejs.image.load('./rect.png');
+    var btAdd = gamejs.image.load('./add.png');
     // create image masks from surface
     var mImg = new pixelcollision.Mask(img);
+    var mBtAdd = new pixelcollision.Mask(btAdd);
+    
     var imgPosition = [90, 0];
+    var btAddPos = [10,10];
     var font = new gamejs.font.Font('20px monospace');
     var dragging = false;
-    var lastMousePos = []
+    var lastMousePos = [];
+    var geos = [];
+    var tipos = []
+    /**
+     * tipos geometricos
+     */
+    function Tipo(nome, img) {
+        this.nome = nome;
+        this.img = gamejs.image.load(img);
+    }
 
+    /**
+     * formas geometricas
+     */
+    function Geo(x, y, tipo) {
+        this.x = x;
+        this.y = y;
+        this.tipo = tipo;
+        
+    }
+
+    tipos[0] = new Tipo("novo",'./rect.png');
+    
+    
     gamejs.event.onMouseDown(function (event) {
         // Creates a retctangle at image position and size
         var rect = new gamejs.Rect([imgPosition[0], imgPosition[1]], mImg.getSize());
@@ -45,10 +71,19 @@ function main() {
             dragging = false;
         }
         
+        var buttonAdd = new gamejs.Rect([btAddPos[0],btAddPos[1]],btAdd.getSize());
+        if (buttonAdd.collidePoint(event.pos)){
+            var relative = $v.subtract(event.pos, [btAddPos[0],btAddPos[1]]);
+            if (mBtAdd.getAt(relative[0],relative[1])){
+               geos[geos.length] = new Geo(Math.floor(Math.random()*50),Math.floor(Math.random()*50),tipos[0]);
+            }
+        
+        }
     });
-
+    
     gamejs.event.onMouseUp(function (event) {
         dragging = false;
+        
     });
 
     gamejs.event.onMouseMotion(function (event) {
@@ -65,7 +100,12 @@ function main() {
     gamejs.onTick(function () {
         // draw
         display.clear();
+        geos.forEach(function (a, i){
+            console.log(geos.length);
+            display.blit(a.tipo.img, [a.x,a.y]);
+        });
         display.blit(img, imgPosition);
+        display.blit(btAdd, btAddPos);
         if (dragging) {
             display.blit(font.render("Dragging the image", '#ff0000'), [300, 20]);
         }
@@ -74,6 +114,7 @@ function main() {
 ;
 
 gamejs.preload([
-    './rect.png'
+    './rect.png',
+    './add.png'
 ]);
 gamejs.ready(main);
